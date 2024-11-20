841e99edecaafa3cf49183d5b8ce2687edc621c8
Alkaish-Husain <alkaishhusain0@gmail.com>
2024-11-20 14:09:39 +0530

    Assignment No:-3

---
+++ 19_Alkaish.c/Assignment_3.c
@@ -0,0 +1,56 @@
+/*
+  Roll No.     :-19
+  Student Name :-Shaikh Alkaish Husain
+  Program Title:-WAP to create a Multiplication of Table by user
+
+  Task Given By:
+    - Roll No. of Assignee:-16
+    - Assignee Name :-Sohan Choudary
+*/
+
+#include <stdio.h>
+
+const_SIZE  // The size of the multiplication table
+
+    // Function to generate multiplication table
+    void generateMultiplicationTable(int *table, int number) {
+  for (int i = 0; i < SIZE; i++) {
+    table[i] = number * (i + 1);  // Calculate the multiplication value
+  }
+}
+
+int main() {
+  int number;
+  int multiplicationTable[SIZE];
+  printf("\t\t\t\t\t*****Multiplication of Table*****\n");
+
+  // Enter Any Number
+  printf("Enter a number to generate its multiplication table:- ");
+  scanf("%d", &number);
+
+  // Generate multiplication table
+  generateMultiplicationTable(multiplicationTable, number);
+
+  // Display the Multiplication table
+  printf("Multiplication table for %d:\n", number);
+  for (int i = 0; i < SIZE; i++) {
+    printf("%d x %d = %d\n", number, i + 1, multiplicationTable[i]);
+  }
+
+  return 0;
+z}
+/*
+Output:-
+                                        *****Multiplication of Table*****
+Enter a number to generate its multiplication table:- 5
+Multiplication table for 5:
+5 x 1 = 5
+5 x 2 = 10
+5 x 3 = 15
+5 x 4 = 20
+5 x 5 = 25
+5 x 6 = 30
+5 x 7 = 35
+5 x 8 = 40
+5 x 9 = 45
+5 x 10 = 50*/
